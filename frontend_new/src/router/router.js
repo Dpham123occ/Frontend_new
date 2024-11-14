2a52ec6dd0cc20b9c760945ab2a59fda0db5154f
Carlosishome <c.i.prudhomme@tcu.edu>
2024-11-14 17:58:19 -0600

    Folder with Acquisistion components added to the router

--- frontend_new/src/router/router.js
+++ frontend_new/src/router/router.js
@@ -3,6 +3,10 @@ import {createRouter, createWebHistory} from "vue-router";
 import App from '../App.vue'
 import ApraisalDistrict from '../components/ApraisalDistrict.vue';
 import AcquisitionList from '../components/AcquisitionList.vue';
+import LightIndustrialMT from "../AcquisitionComponents/LightIndustrialMT.vue";
+import LightIndustrialUB from "../AcquisitionComponents/LightIndustrialUB.vue";
+import IOSExsisting from "../AcquisitionComponents/IOSExsisting.vue";
+import IOSDevelopment from "../AcquisitionComponents/IOSDevelopment.vue";
 
 const routes = [
     {
@@ -24,8 +28,32 @@ const routes = [
     path: '/acquisitionlist',
     name: 'Acquisition List',
     component: AcquisitionList,
-    }
-]
+  },
+    
+  {
+    path: '/acquisition-list/LightIndustrialMT', // Update to match component route
+    name: 'Light Industrial Multi Tenant',
+    component: LightIndustrialMT,
+  },
+
+ {
+    path: '/acquisition-list/LightIndustrialUB', // Update to match component route
+    name: 'Light Industrial User Building',
+    component: LightIndustrialUB,
+  },
+  {
+    path: '/acquisition-list/IOSExsiting', // Update to match component route
+    name: 'IOS Exsiting',
+    component: IOSExsisting,
+  },
+
+    {
+    path: '/acquisition-list/IOSDevelopment', // Update to match component route
+    name: 'IOS Development',
+    component: IOSDevelopment,
+  },
+
+];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
