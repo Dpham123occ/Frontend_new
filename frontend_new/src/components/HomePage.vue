0a90105b62580db99f49d61d4432d094834986d9
Duy Pham <duyduy456@gmail.com>
2025-01-21 09:45:40 -0600

    changing homepage

--- frontend_new/src/components/HomePage.vue
+++ frontend_new/src/components/HomePage.vue
@@ -7,10 +7,25 @@
 
 <script>
 import Sidebar from './Sidebar.vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
 export default {
   name: 'HomePage',
   components: {
     Sidebar,
+  },
+  setup() {
+    const { handleRedirectCallback } = useAuth0();
+    const route = useRoute();
+
+    onMounted(async () => {
+      if (route.query.code && route.query.state) {
+        // Auth0 handles the callback and completes login
+        await handleRedirectCallback();
+      }
+    });
   },
 };
 </script>
